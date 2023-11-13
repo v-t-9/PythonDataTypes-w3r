3b50f233ae21abc8ab1dbedeb4fc01c5519c1587
v-t-9 <violeta.a.t.9@gmail.com>
2023-11-13 18:07:54 -0300

    ex76

--- List/ex76.py
+++ List/ex76.py
@@ -1,3 +1,4 @@
+from ex75 import runlen_encoding, str_to_list
 # Write a Python program to create a list reflecting the modified run-length encoding from a given list of integers or a given list of characters.
 # Original list:
 # [1, 1, 2, 3, 4, 4, 5, 1]
@@ -7,3 +8,26 @@
 # aabcddddadnss
 # List reflecting the modified run-length encoding from the said string:
 # [[2, 'a'], 'b', 'c', [4, 'd'], 'a', 'd', 'n', [2, 's']]
+
+
+def modified_encoding(l):
+    li = runlen_encoding(l)
+    res = []
+    for i in li: 
+        if type(i) == list:
+            if i[0] == 1:
+                res.append(i[1])
+            else:
+                res.append(i)
+    return res
+if __name__ == "__main__":
+    l = [1, 1, 2, 3, 4, 4, 5, 1]
+    s = "aabcddddadnss"
+    l1 = str_to_list(s)
+    print(modified_encoding(l))
+    print(modified_encoding(l1))
+
+  
+
+       
+
